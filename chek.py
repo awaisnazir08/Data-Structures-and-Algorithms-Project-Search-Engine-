12b1b1143cb610bd2c8d014cba9d41bcaf5150b6
awaisnazir08 <owaisnazir2228@gmail.com>
2023-12-09 21:09:22 +0500

    Alhamdullilah basic search engine wdout pagerank

---
+++ chek.py
@@ -0,0 +1,5 @@
+import json
+file_path = "Forward_Index/Lexicon.json"
+with open(file_path, "r") as file:
+    data =  json.load(file)
+print('done\nhello\nfgsdrgdg')
---
+++ search engine.py
@@ -0,0 +1,100 @@
+import json
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import re
+
+def load_Lexicon():
+    file_path = "Forward_Index/Lexicon.json"
+    try:
+        with open(file_path, "r") as file:
+            return json.load(file)
+    except (FileExistsError, FileNotFoundError):
+        return None
+
+def load_documentIndex():
+    file_path = "Forward_Index/document_index.json"
+    try:
+        with open(file_path, "r") as file:
+            return json.load(file)
+    except (FileExistsError, FileNotFoundError):
+        return None
+
+def load_document_date_file():
+    file_path = "Forward_Index/docId_date_mapping.json"
+    try:
+        with open(file_path, "r") as file:
+            return json.load(file)
+    except (FileExistsError, FileNotFoundError):
+        return None
+
+def load_inverted_index_barrel(path):
+    try: 
+        with open(path, "r") as file:
+            return json.load(file)
+    except (FileExistsError, FileNotFoundError):
+        return None
+
+def get_document_ids(word_id, word_data_in_barrel):
+    documents = {}
+    if str(word_id) in word_data_in_barrel["word_ID"]:
+        word_data = word_data_in_barrel["word_ID"][str(word_id)]
+        # document_ids = list(word_data.keys())
+        return word_data
+    else:
+        return {}
+
+
+
+# class SearchEngine:
+#     def __init__(self):
+
+
+
+#setting up the english stop words from the NLTK
+stop_words = set(stopwords.words('english'))
+
+#object for the lemmatizer class
+lemmatizer = WordNetLemmatizer()
+
+inverted_index_file_paths = []
+for i in range(1, 101):
+    inverted_index_file_paths.append(f'Inverted_Index/Inverted_index_files/inverted_index_barrel_{i}.json')
+
+lexicon_dictionary = load_Lexicon()
+
+document_urls = load_documentIndex()
+
+
+
+query = input("Enter the query to search: ")
+
+#tokenizing the combined words
+query_tokenized = word_tokenize(query)
+
+# Remove special characters, dots, etc.
+query_tokenized = [word for word in query_tokenized if re.match("^[a-zA-Z0-9_]*$", word)]
+
+# Convert to lowercase
+query_tokenized = [word.lower() for word in query_tokenized]
+
+# Remove stop words
+clean_query = [word for word in query_tokenized if word not in stop_words]
+
+# Lemmatization
+clean_query = [lemmatizer.lemmatize(word) for word in clean_query]
+
+for word in clean_query:
+    word_id = lexicon_dictionary[word]
+    print(word_id)
+    barrel_id = word_id % 100
+    print(barrel_id + 1)
+    word_data_in_barrel = load_inverted_index_barrel(f'Inverted_Index/Inverted_index_files/inverted_index_barrel_{barrel_id + 1}.json')
+    documents = get_document_ids(word_id, word_data_in_barrel)
+    for document_id in documents.keys():
+        document_url = document_urls[document_id]
+        print(document_url)
+    # print(documents.keys())
+    # for document in documents:
+    #     document_id = documents[document.key]
+    #     print(document_id)
